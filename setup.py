fc7bf890670b16a428bdcd3cc3cdb1c9771b98f7
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-02 15:52:47 +0100

    New version 0.0.9

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classifiers = [s.strip() for s in classes.split('\n') if s]
 
 setup(
     name='tux-control-plugin-xscreensaver',
-    version='0.0.8',
+    version='0.0.9',
     description='Tux Control XScreensaver plugin',
     long_description=open('README.md').read(),
     author='Adam Schubert',
